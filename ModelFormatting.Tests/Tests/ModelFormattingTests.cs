51a261b98253703ad6e5c13d6babae725b2c9e8b
bresheske <bresheske@gmail.com>
2013-12-18 09:35:50 -0600

    All tests passing again.

--- ModelFormatting.Tests/Tests/ModelFormattingTests.cs
+++ ModelFormatting.Tests/Tests/ModelFormattingTests.cs
@@ -1,19 +1,26 @@
-﻿using ModelFormatting.Extensions;
-using ModelFormatting.Tests.Models;
+﻿using ModelFormatting.Tests.Models;
+using ModelFormatting.Services;
 using NUnit.Framework;
 using System;
 
 namespace ModelFormatting.Tests
 {
     [TestFixture]
-    public class ModelFormattingExtensionsTests
+    public class ModelFormattingTests
     {
+        private IModelFormatter formatter;
+
+        [TestFixtureSetUp]
+        public void Init()
+        {
+            formatter = new DefaultModelFormatter(new DefaultFormatParser(), new DefaultKeyFormatPrecedenceProvider());
+        }
 
         [Test]
         public void SimpleFormatting()
         {
             var obj = new { Name = "Scott", Address = "101 Elm Street"};
-            var format = obj.FormatModel("Name: {Name} Address: {Address}");
+            var format = formatter.FormatModel(obj, "Name: {Name} Address: {Address}");
             Assert.AreEqual("Name: Scott Address: 101 Elm Street", format);
         }
 
@@ -21,32 +28,32 @@ namespace ModelFormatting.Tests
         public void NumberFormatting()
         {
             var obj = new { Name = "Scott", Money = 2m };
-            var format = obj.FormatModel("Name: {Name} Money: {Money:C}");
+            var format = formatter.FormatModel(obj, "Name: {Name} Money: {Money:C}");
             Assert.AreEqual("Name: Scott Money: $2.00", format);
 
             var decobj = new {Name = "Nancy", Number = 2000};
-            Assert.AreEqual("Name: Nancy Number: 2000.00", 
-                decobj.FormatModel("Name: {Name} Number: {Number:F}"));
+            Assert.AreEqual("Name: Nancy Number: 2000.00",
+                formatter.FormatModel(decobj, "Name: {Name} Number: {Number:F}"));
             Assert.AreEqual("Name: Nancy Number: 7D0",
-                decobj.FormatModel("Name: {Name} Number: {Number:X}"));
+                formatter.FormatModel(decobj, "Name: {Name} Number: {Number:X}"));
             Assert.AreEqual("Name: Nancy Number: 2000.0",
-                decobj.FormatModel("Name: {Name} Number: {Number:0.0}"));
+                formatter.FormatModel(decobj, "Name: {Name} Number: {Number:0.0}"));
             Assert.AreEqual("Name: Nancy Number: (2000)",
-                decobj.FormatModel("Name: {Name} Number: {Number:(#).##}"));
+                formatter.FormatModel(decobj, "Name: {Name} Number: {Number:(#).##}"));
             Assert.AreEqual("Name: Nancy Number: (2000).00",
-                decobj.FormatModel("Name: {Name} Number: {Number:(#).00}"));
+                formatter.FormatModel(decobj, "Name: {Name} Number: {Number:(#).00}"));
             Assert.AreEqual("Name: Nancy Number: 200000%",
-                decobj.FormatModel("Name: {Name} Number: {Number:0%}"));
+                formatter.FormatModel(decobj, "Name: {Name} Number: {Number:0%}"));
         }
 
         [Test]
         public void DateFormatting()
         {
             var obj = new { Name = "Scott", Date = new DateTime(2012, 11, 13) };
-            Assert.AreEqual("Name: Scott Date: 11/13/2012", 
-                obj.FormatModel("Name: {Name} Date: {Date:d}"));
+            Assert.AreEqual("Name: Scott Date: 11/13/2012",
+                formatter.FormatModel(obj, "Name: {Name} Date: {Date:d}"));
             Assert.AreEqual("Name: Scott Date: Tuesday, November 13, 2012",
-                obj.FormatModel("Name: {Name} Date: {Date:D}"));
+                formatter.FormatModel(obj, "Name: {Name} Date: {Date:D}"));
         }
 
         [Test]
@@ -60,8 +67,8 @@ namespace ModelFormatting.Tests
                 Ticks = 1504388483838394594L,
                 SomeInt = 12
             };
-            Assert.AreEqual("$_()/@1 11/13/2012 $24.54 1504388483838394594.00 12", 
-                obj.FormatModel("{Name10} {Date:d} {Money:C} {Ticks:0.00} {SomeInt}"));
+            Assert.AreEqual("$_()/@1 11/13/2012 $24.54 1504388483838394594.00 12",
+                formatter.FormatModel(obj, "{Name10} {Date:d} {Money:C} {Ticks:0.00} {SomeInt}"));
         }
 
         [Test]
@@ -74,10 +81,10 @@ namespace ModelFormatting.Tests
                 Name = "Bobby"
             };
 
-            Assert.AreEqual("Name: Bobby BirthDate: 4/3/1987 Age: 25.00", 
-                obj.FormatModel("Name: {Name} BirthDate: {BirthDate} Age: {Age}"));
+            Assert.AreEqual("Name: Bobby BirthDate: 4/3/1987 Age: 25.00",
+                formatter.FormatModel(obj, "Name: {Name} BirthDate: {BirthDate} Age: {Age}"));
             Assert.AreEqual("Name: Bobby BirthDate: 4/3/1987 Age: 25.0000",
-                obj.FormatModel("Name: {Name} BirthDate: {BirthDate} Age: {Age:0.0000}"));
+                formatter.FormatModel(obj, "Name: {Name} BirthDate: {BirthDate} Age: {Age:0.0000}"));
         }
 
         [Test]
@@ -91,11 +98,11 @@ namespace ModelFormatting.Tests
             };
 
             Assert.AreEqual("Name: Bobby BirthDate: 4/3/1987 Age: 25.00 {Oranges}",
-                obj.FormatModel("Name: {Name} BirthDate: {BirthDate} Age: {Age} {Oranges}"));
+                formatter.FormatModel(obj, "Name: {Name} BirthDate: {BirthDate} Age: {Age} {Oranges}"));
             Assert.AreEqual("Name: Bobby BirthDate: 4/3/1987 Age: 25.00 {{Oranges}}",
-                obj.FormatModel("Name: {Name} BirthDate: {BirthDate} Age: {Age} {{Oranges}}"));
+                formatter.FormatModel(obj, "Name: {Name} BirthDate: {BirthDate} Age: {Age} {{Oranges}}"));
             Assert.AreEqual("Name: Bobby BirthDate: 4/3/1987 Age: 25.00 {SomeJsonObject: 'Organic'}",
-                obj.FormatModel("Name: {Name} BirthDate: {BirthDate} Age: {Age} {SomeJsonObject: 'Organic'}"));
+                formatter.FormatModel(obj, "Name: {Name} BirthDate: {BirthDate} Age: {Age} {SomeJsonObject: 'Organic'}"));
         }
     }
 }
